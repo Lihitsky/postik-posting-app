029096dc43c9131631cd09b5a0f6444262d31257
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-08-29 23:52:41 +0300

    feat(components): update Header to support light and dark mode with dynamic icons

--- react-client/src/components/header/index.tsx
+++ react-client/src/components/header/index.tsx
@@ -1,21 +1,33 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from '../theme-provider'
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
-import LogoBlack from '../../assets/svg/PostikLogoBig-B.svg';
+import React, { useContext } from "react"
+import { ThemeContext } from "../theme-provider"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react"
+import LogoBlack from "../../assets/svg/PostikLogoBig-B.svg"
+import LogoWhite from "../../assets/svg/PostikLogoBig-W.svg"
+import { FaRegMoon } from "react-icons/fa"
+import { LuSunMedium } from "react-icons/lu"
 
 export const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
 
   return (
     <Navbar>
-        <NavbarBrand>
-            <img src={LogoBlack}></img>
-        </NavbarBrand>
-        <NavbarContent justify='end'>
-            <NavbarItem className='lg:flex text-3xl cursor-pointer'>
-                
-            </NavbarItem>    
-        </NavbarContent>
+      <NavbarBrand>
+        <img src={theme === "light" ? LogoBlack : LogoWhite}></img>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem
+          className="lg:flex text-3xl cursor-pointer"
+          onClick={() => toggleTheme()}
+        >
+          {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
+        </NavbarItem>
+        <NavbarItem></NavbarItem>
+      </NavbarContent>
     </Navbar>
   )
 }
