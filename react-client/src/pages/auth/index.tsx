d5bcad2f07e7028bd1d7535367402f734fbad4b4
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-08-31 22:49:42 +0300

    feat(pages): update Auth component with tab navigation for Login and Register

--- react-client/src/pages/auth/index.tsx
+++ react-client/src/pages/auth/index.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react"
+import React, { useState } from "react"
+import { Login } from "../../features/user/login"
+import { Register } from "../../features/user/register"
 
 export const Auth = () => {
+  const [selected, setSelected] = useState("sign in")
+
   return (
-    <div>Auth</div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overflow-hidden">
+            <Tabs
+              fullWidth
+              size="md"
+              selectedKey={selected}
+              onSelectionChange={key => setSelected(key as string)}
+            >
+              <Tab key="sign in" title="Sign in">
+                <Login setSelected={setSelected} />
+              </Tab>
+              <Tab key="sign up" title="Sign up">
+                <Register setSelected={setSelected}/>
+              </Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
   )
 }
