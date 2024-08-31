bcbad1ac35d1e2b627d548e2eaaedb86bb0ed961
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-08-31 22:50:41 +0300

    feat(features): add user slice with authentication and user state management

---
+++ react-client/src/features/user/userSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { User } from "../../app/types"
+import { build } from "vite"
+import { userApi } from "../../app/services/userApi"
+
+interface InitialState {
+  user: User | null
+  isAuthentificated: boolean
+  users: User[] | null
+  current: User | null
+  token?: string
+}
+
+const initialState: InitialState = {
+  user: null,
+  isAuthentificated: false,
+  users: null,
+  current: null,
+}
+
+const slice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    logout: () => initialState,
+    resetUser: state => {
+      state.user = null
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addMatcher(userApi.endpoints.login.matchFulfilled, (state, action) => {
+        state.token = action.payload.token
+        state.isAuthentificated = true
+      })
+      .addMatcher(userApi.endpoints.current.matchFulfilled, (state, action) => {
+        state.isAuthentificated = true
+        state.current = action.payload
+      })
+      .addMatcher(
+        userApi.endpoints.getUserById.matchFulfilled,
+        (state, action) => {
+          state.user = action.payload
+        },
+      )
+  },
+})
+
+export const { logout, resetUser } = slice.actions;
+
+export default slice.reducer
+
