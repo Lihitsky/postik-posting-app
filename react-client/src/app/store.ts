01560cea3b8cf62e34bee8b1501f483d0df169f0
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-08-31 22:53:49 +0300

    feat(store): extend Redux store with user slice and authentication middleware

--- react-client/src/app/store.ts
+++ react-client/src/app/store.ts
@@ -1,13 +1,21 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { configureStore } from "@reduxjs/toolkit"
 import { api } from "./services/api"
+import user from "../features/user/userSlice"
+import { listenerMiddleware } from "../middleware/auth"
 
 // The store setup is wrapped in `makeStore` to allow reuse
 // when setting up tests that need the same store config
 
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer
+    [api.reducerPath]: api.reducer,
+    user,
+  },
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware()
+      .concat(api.middleware)
+      .prepend(listenerMiddleware.middleware)
   },
 })
 
