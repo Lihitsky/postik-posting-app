b79deb477c6142a54bf6965c4cfa381b55d8b5b6
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-09-01 19:18:29 +0300

    feat(components): add Profile component to Layout

--- react-client/src/components/layout/index.tsx
+++ react-client/src/components/layout/index.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react'
-import { Header } from '../header'
-import { Container } from '../container'
-import { NavBar } from '../nav-bar'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectIsAuthentificated, selectUser } from '../../features/user/userSlice'
+import React, { useEffect } from "react"
+import { Header } from "../header"
+import { Container } from "../container"
+import { NavBar } from "../nav-bar"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import {
+  selectIsAuthentificated,
+  selectUser,
+} from "../../features/user/userSlice"
+import { Profile } from "../profile"
 
 export const Layout = () => {
   const isAuthenticated = useSelector(selectIsAuthentificated)
@@ -13,21 +17,24 @@ export const Layout = () => {
 
   useEffect(() => {
     if (!isAuthenticated) {
-      navigate('/auth')
+      navigate("/auth")
     }
   }, [])
 
   return (
     <>
-        <Header />
-        <Container>
-            <div className='flex-2 p-4'>
-                <NavBar />
-            </div>
-            <div className='flex-1 p-4'>
-                <Outlet />
-            </div>
-        </Container>
+      <Header />
+      <Container>
+        <div className="flex-2 p-4">
+          <NavBar />
+        </div>
+        <div className="flex-1 p-4">
+          <Outlet />
+        </div>
+        <div className="flex-2 p-4">
+          <div className="flex-col flex gap-5">{!user && <Profile />}</div>
+        </div>
+      </Container>
     </>
   )
 }
