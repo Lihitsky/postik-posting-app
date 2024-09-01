d0c4c3b7a4c81130b479fee4e1bdf46d913ab63f
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-09-01 19:17:34 +0300

    feat(components): add logout button to Header

--- react-client/src/components/header/index.tsx
+++ react-client/src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react"
 import { ThemeContext } from "../theme-provider"
 import {
+  Button,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -10,9 +11,22 @@ import LogoBlack from "../../assets/svg/PostikLogoBig-B.svg"
 import LogoWhite from "../../assets/svg/PostikLogoBig-W.svg"
 import { FaRegMoon } from "react-icons/fa"
 import { LuSunMedium } from "react-icons/lu"
+import { useDispatch, useSelector } from "react-redux"
+import { logout, selectIsAuthentificated } from "../../features/user/userSlice"
+import { useNavigate } from "react-router-dom"
+import { CiLogout } from "react-icons/ci"
 
 export const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
+  const isAuthentificated = useSelector(selectIsAuthentificated)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem("token")
+    navigate("/auth")
+  }
 
   return (
     <Navbar>
@@ -26,7 +40,18 @@ export const Header = () => {
         >
           {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
         </NavbarItem>
-        <NavbarItem></NavbarItem>
+        <NavbarItem>
+          {isAuthentificated && (
+            <Button
+              color="default"
+              variant="flat"
+              className="gap-2"
+              onClick={handleLogout}
+            >
+              <CiLogout /> <span>Logout</span>
+            </Button>
+          )}
+        </NavbarItem>
       </NavbarContent>
     </Navbar>
   )
